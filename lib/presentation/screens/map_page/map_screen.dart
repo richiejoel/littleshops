e8042cbad9ac88d15fc63321b5b64391e87965d4
Richard Joel Garcia <34136964+richiejoel@users.noreply.github.com>
2021-08-28 14:54:09 -0500

    Custom marker flutter :)

--- lib/presentation/screens/map_page/map_screen.dart
+++ lib/presentation/screens/map_page/map_screen.dart
@@ -24,6 +24,7 @@ class MapScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Google Maps Demo',
+      debugShowCheckedModeBanner: false,
       home: MapSample(),
     );
   }
@@ -110,7 +111,8 @@ class MapSampleState extends State<MapSample> {
           markerId: MarkerId(busy.id),
           position: LatLng(busy.latitude, busy.longitude),
           infoWindow: InfoWindow(title: busy.name),
-          icon: await getMarkerImageFromUrl(busy.imageUrl, targetWidth: 100),
+          //icon: await getMarkerImageFromUrl(busy.imageUrl, targetWidth: 100),
+          icon: await getMarkerIcon(busy.imageUrl, Size(100.0, 100.0)),
       );
       markersList.add(marker!);
        setState(() {
@@ -157,6 +159,7 @@ class MapSampleState extends State<MapSample> {
                   onPressed: _goToTheLake,
                   label: Text('To the lake!'),
                   icon: Icon(Icons.directions_boat),
+                  backgroundColor: COLOR_CONST.primaryColor,
                 ),
               ),
             );
@@ -291,5 +294,139 @@ class MapSampleState extends State<MapSample> {
 
     return Images.encodePng(markerImage!);
   }
+
+  ///ini rjgman
+
+  Future<ui.Image> getImageFromPath(String imagePath) async {
+    final File imageFile = await
+    DefaultCacheManager().getSingleFile(imagePath);
+    //File imageFile = File(imagePath);
+
+    Uint8List imageBytes = imageFile.readAsBytesSync();
+
+    final Completer<ui.Image> completer = new Completer();
+
+    ui.decodeImageFromList(imageBytes, (ui.Image img) {
+      return completer.complete(img);
+    });
+
+    return completer.future;
+  }
+
+  Future<BitmapDescriptor> getMarkerIcon(String imagePath, Size size) async {
+    final ui.PictureRecorder pictureRecorder = ui.PictureRecorder();
+    final Canvas canvas = Canvas(pictureRecorder);
+
+    final Radius radius = Radius.circular(size.width / 2);
+
+    final Paint tagPaint = Paint()..color = COLOR_CONST.discountColor;
+    final double tagWidth = 40.0;
+
+    final Paint shadowPaint = Paint()..color = COLOR_CONST.primaryColor.withAlpha(170);
+    final double shadowWidth = 15.0;
+
+    final Paint borderPaint = Paint()..color = Colors.white;
+    final double borderWidth = 3.0;
+
+    final double imageOffset = shadowWidth + borderWidth;
+
+    // Add shadow circle
+    canvas.drawRRect(
+        RRect.fromRectAndCorners(
+          Rect.fromLTWH(
+              0.0,
+              0.0,
+              size.width,
+              size.height
+          ),
+          topLeft: radius,
+          topRight: radius,
+          bottomLeft: radius,
+          bottomRight: radius,
+        ),
+        shadowPaint);
+
+    // Add border circle
+    canvas.drawRRect(
+        RRect.fromRectAndCorners(
+          Rect.fromLTWH(
+              shadowWidth,
+              shadowWidth,
+              size.width - (shadowWidth * 2),
+              size.height - (shadowWidth * 2)
+          ),
+          topLeft: radius,
+          topRight: radius,
+          bottomLeft: radius,
+          bottomRight: radius,
+        ),
+        borderPaint);
+
+    // Add tag circle
+    canvas.drawRRect(
+        RRect.fromRectAndCorners(
+          Rect.fromLTWH(
+              size.width - tagWidth,
+              0.0,
+              tagWidth,
+              tagWidth
+          ),
+          topLeft: radius,
+          topRight: radius,
+          bottomLeft: radius,
+          bottomRight: radius,
+        ),
+        tagPaint);
+
+    // Add tag text
+    TextPainter textPainter = TextPainter(textDirection: TextDirection.ltr);
+    textPainter.text = TextSpan(
+      text: '1',
+      style: TextStyle(fontSize: 20.0, color: Colors.white),
+    );
+
+    textPainter.layout();
+    textPainter.paint(
+        canvas,
+        Offset(
+            size.width - tagWidth / 2 - textPainter.width / 2,
+            tagWidth / 2 - textPainter.height / 2
+        )
+    );
+
+    // Oval for the image
+    Rect oval = Rect.fromLTWH(
+        imageOffset,
+        imageOffset,
+        size.width - (imageOffset * 2),
+        size.height - (imageOffset * 2)
+    );
+
+    // Add path for oval image
+    canvas.clipPath(Path()
+      ..addOval(oval));
+
+    // Add image
+    ui.Image image = await getImageFromPath(imagePath); // Alternatively use your own method to get the image
+    paintImage(canvas: canvas, image: image, rect: oval, fit: BoxFit.fitWidth);
+
+    // Convert canvas to image
+    final ui.Image markerAsImage = await pictureRecorder.endRecording().toImage(
+        size.width.toInt(),
+        size.height.toInt()
+    );
+
+    // Convert image to bytes
+    final ByteData? byteData = await markerAsImage.toByteData(format: ui.ImageByteFormat.png);
+    final Uint8List uint8List = byteData!.buffer.asUint8List();
+
+    return BitmapDescriptor.fromBytes(uint8List);
+  }
+
+  ///fin rjgman
+
+  ///INI RJGMAN
+
+  /// FIN RJGMAN
 
 }
